18df3706dc943bde4376247561c44e9045c8c3d0
Joson5 <wangsanmu5@163.com>
2020-11-24 20:07:38 +0800

    sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,50 +16,65 @@ module.exports = {
     //   ['/guide/react/docs/main-concepts/composition-vs-inheritance', '组合VS继承'],
     //   ['/guide/react/docs/main-concepts/thinking-in-react', 'react哲学'],
     // ],
-    sidebar: [
-      {
-        title: 'react',
-        path: '/guide/react/',
-        collapsable: true,
-        sidebarDepth: 1,
-        children: [
-          {
-            title: '安装',
-            path: '/guide/react/docs/install/',
-            collapsable: true,
-            sidebarDepth: 1,
-            children: [
-              {
-                title: '开始',
-                path: '/guide/react/docs/install/started',
-                collapsable: false,
-                sidebarDepth: 1,
-              }
-            ]
-          },
-          {
-            title: '核心',
-            path: '/guide/react/docs/main-concepts/',
-            collapsable: true,
-            sidebarDepth: 1,
-            children: [
-              {
-                title: '组合VS继承',
-                path: '/guide/react/docs/main-concepts/composition-vs-inheritance',
-                collapsable: false,
-                sidebarDepth: 1,
-              },
-              {
-                title: 'react哲学',
-                path: '/guide/react/docs/main-concepts/thinking-in-react',
-                collapsable: false,
-                sidebarDepth: 1,
-              },
-            ]
-          }
-        ]
-      },
-    ],
+    // sidebar: [
+    //   {
+    //     title: 'react',
+    //     path: '/guide/react/',
+    //     collapsable: true,
+    //     sidebarDepth: 1,
+    //     children: [
+    //       {
+    //         title: '安装',
+    //         path: '/guide/react/docs/install/',
+    //         collapsable: true,
+    //         sidebarDepth: 1,
+    //         children: [
+    //           {
+    //             title: '开始',
+    //             path: '/guide/react/docs/install/started',
+    //             collapsable: false,
+    //             sidebarDepth: 1,
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         title: '核心',
+    //         path: '/guide/react/docs/main-concepts/',
+    //         collapsable: true,
+    //         sidebarDepth: 1,
+    //         children: [
+    //           {
+    //             title: '组合VS继承',
+    //             path: '/guide/react/docs/main-concepts/composition-vs-inheritance',
+    //             collapsable: false,
+    //             sidebarDepth: 1,
+    //           },
+    //           {
+    //             title: 'react哲学',
+    //             path: '/guide/react/docs/main-concepts/thinking-in-react',
+    //             collapsable: false,
+    //             sidebarDepth: 1,
+    //           },
+    //         ]
+    //       }
+    //     ]
+    //   },
+    // ],
+    sidebar: {
+      '/guide/react/': [
+        '/guide/react/docs/install/started',
+        '/guide/react/docs/main-concepts/composition-vs-inheritance',
+        '/guide/react/docs/main-concepts/thinking-in-react'
+      ],
+
+      '/guide/npm/': [
+        '/guide/npm/started',
+      ],
+
+      '/guide/module/': [
+        '/guide/module/module',
+      ]
+    },
     lastUpdated: 'Last Updated', // string | boolean
     repo: 'JosonKing/josonking.github.io',
     repoLabel: 'Github',
